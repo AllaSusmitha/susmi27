ad8e31e4bfd3bb7e20bd98c109be758177e9ad3b
AllaSusmitha <41768199+AllaSusmitha@users.noreply.github.com>
2018-08-06 14:59:43 +0530

    Update susmi27.py

--- susmi27.py
+++ susmi27.py
@@ -1,5 +1,5 @@
-N,Q=map(int,raw_input().split())
-if N <= 100000 and Q <= 100000:
-	for i in range(N+1,Q+1):
+N,K=map(int,raw_input().split())
+if N <= 100000 and K <= 100000:
+	for i in range(N+1,K+1):
 		if (i%2 != 0):
 			print i,
